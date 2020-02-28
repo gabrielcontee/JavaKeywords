a25fea2c5b0e8757bdc630347806b5216a4f0919
Gabriel Conte <gabrielconte14@gmail.com>
2020-02-28 14:39:22 -0300

    Xcode project folders organization

--- JavaKeywords/JavaKeywords/Flow/KeywordsViewController.swift
+++ JavaKeywords/JavaKeywords/Flow/KeywordsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  KeywordsViewController.swift
 //  JavaKeywords
 //
 //  Created by Gabriel Conte on 28/02/20.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class KeywordsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
 }
-
